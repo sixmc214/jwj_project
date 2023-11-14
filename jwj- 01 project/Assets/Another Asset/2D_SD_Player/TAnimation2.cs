7c21be92aba3db8d2bef3e5023df4a6e0f70e1b4
yukino <jwj2147@gmail.com>
2023-11-14 12:53:22 +0900

    에셋추가

---
+++ jwj- 01 project/Assets/Another Asset/2D_SD_Player/TAnimation2.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TAnimation2 : MonoBehaviour {
+
+    Animator anim;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (anim.GetBool("isAttack") == false)
+            {
+                anim.SetBool("isAttack", true);
+                anim.SetBool("isIdle", false);
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (anim.GetBool("isHited") == false)
+            {
+                anim.SetBool("isHited", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (anim.GetBool("isDied") == false)
+            {
+                anim.SetBool("isDied", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            if (anim.GetBool("isRun") == false)
+            {
+                anim.SetBool("isRun", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else if (Input.GetKey(KeyCode.G))
+        {
+            if (anim.GetBool("isVictory") == false)
+            {
+                anim.SetBool("isVictory", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else
+        {
+            if (anim.GetBool("isIdle") == false)
+            {
+                anim.SetBool("isHited", false);
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isAttack", false);
+                anim.SetBool("isVictory", false);
+                anim.SetBool("isDied", false);
+                anim.SetBool("isRun", false);
+            }
+        }
+    }
+}
